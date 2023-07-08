491262cfe0396d31b4660fc7e52ad6d85afc23b9
Nguyen Do <khoanguyen.do@outlook.com>
2023-07-09 01:01:11 +0700

    ci: apply nuxt ssr on netlify (#1)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   modules: ["@nuxtjs/google-fonts", "@nuxthq/ui"],
   runtimeConfig: {
     public: {
