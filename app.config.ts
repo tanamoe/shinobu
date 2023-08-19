e861a79c41fbcb87a35ab33c7a93348e9e93355d
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-08-20 00:43:54 +0700

    feat: implement editor for title entries

--- app.config.ts
+++ app.config.ts
@@ -2,5 +2,15 @@ export default defineAppConfig({
   ui: {
     primary: "sky",
     gray: "zinc",
+    table: {
+      td: {
+        base: "whitespace-normal",
+      },
+    },
+    button: {
+      variant: {
+        soft: "text-{color}-500 dark:text-{color}-400 bg-{color}-100 hover:bg-{color}-100 disabled:bg-{color}-50 dark:bg-{color}-800 dark:hover:bg-{color}-900 dark:disabled:bg-{color}-950 focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-{color}-500 dark:focus-visible:ring-{color}-400",
+      },
+    },
   },
 });
