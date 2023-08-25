8b6ec29cd3543e77480e64a4eb16f07bc36d8b9a
Nguyen Do <khoanguyen.do@outlook.com>
2023-08-25 09:23:27 +0700

    feat: implement update release (#16)
    * feat: implement update release
    
    * build: add editorconfig
    
    * fix: use enum for releaseStatus

--- composables/release.ts
+++ composables/release.ts
@@ -20,7 +20,6 @@ export const useCreateRelease = () => {
         .create<ReleaseResponse>(release);
 
       toast.add({
-        id: "create_release",
         title: `Success`,
         icon: "i-fluent-checkmark-circle-20-filled",
         color: "green",
@@ -42,4 +41,45 @@ export const useCreateRelease = () => {
   }
 
   return { pending, create };
+};
+
+export const useUpdateRelease = () => {
+  const { $pb } = useNuxtApp();
+  const toast = useToast();
+
+  const pending = ref(false);
+
+  async function update(
+    id: string,
+    release: Partial<ReleaseRecord> | FormData,
+  ) {
+    pending.value = true;
+
+    try {
+      const res = await $pb
+        .collection(Collections.Release)
+        .update<ReleaseResponse>(id, release);
+
+      toast.add({
+        title: `Success`,
+        icon: "i-fluent-checkmark-circle-20-filled",
+        color: "green",
+      });
+
+      return res;
+    } catch (error) {
+      if (error instanceof ClientResponseError)
+        toast.add({
+          title: "Error",
+          description: error.message,
+          icon: "i-fluent-error-circle-20-filled",
+          color: "red",
+        });
+      else console.error(error);
+    } finally {
+      pending.value = false;
+    }
+  }
+
+  return { pending, update };
 };
