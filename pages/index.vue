d7a31bc264b3603d3ae9efaef8f91971b5fb94c1
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-09-10 03:16:42 +0700

    feat(index): implement show random gif from tenor

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+const { data: gif } = await useFetch("/api/random-gif");
+</script>
+
 <template>
   <div class="flex items-center justify-center h-full">
-    <img src="/kikuri.gif" />
+    <img :src="gif?.result" />
   </div>
 </template>
