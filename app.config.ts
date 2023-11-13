f8da5a10ca5c23320318d7b001330afbc8a0b9ca
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-11-13 22:04:42 +0700

    feat(title): implement adding links

--- app.config.ts
+++ app.config.ts
@@ -12,5 +12,10 @@ export default defineAppConfig({
         soft: "text-{color}-500 dark:text-{color}-400 bg-{color}-100 hover:bg-{color}-100 disabled:bg-{color}-50 dark:bg-{color}-800 dark:hover:bg-{color}-900 dark:disabled:bg-{color}-950 focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-{color}-500 dark:focus-visible:ring-{color}-400",
       },
     },
+    card: {
+      body: {
+        padding: "p-3 sm:p-3",
+      },
+    },
   },
 });
