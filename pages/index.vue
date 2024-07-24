54a2857c9e8d8745c8dc73a0d62fe8ad776a04b4
Nguyen Do <khoanguyen.do@outlook.com>
2024-07-24 09:16:02 +0700

    feat: remove random gif

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-const { data: gif } = await useFetch("/api/random-gif");
-</script>
-
 <template>
-  <div class="flex items-center justify-center h-full">
-    <img v-if="gif" :src="gif.result" />
-  </div>
+  <div class="flex items-center justify-center h-full"></div>
 </template>
