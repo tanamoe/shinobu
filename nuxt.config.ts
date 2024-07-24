05cb343cf1b18e3630912a663a1b064cf7acd4d1
Nguyen Do <khoanguyen.do@outlook.com>
2024-07-24 09:15:37 +0700

    build: upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@vueuse/nuxt", "@nuxt/ui"],
+  modules: ["@vueuse/nuxt", "@nuxt/ui", "@pinia/nuxt"],
+
   runtimeConfig: {
     tenorApiKey: process.env.TENOR_API_KEY,
     public: {
       pocketbaseUrl: process.env.POCKETBASE_URL,
     },
   },
+
   typescript: {
     shim: false,
   },
+
   css: ["~/assets/fonts/inter.css"],
   devtools: { enabled: true },
+
   ui: {
     icons: ["fluent"],
   },
+
   experimental: {
     appManifest: false,
   },
+
+  compatibilityDate: "2024-07-09",
 });
