29c6a2c577101cb78e7b5d4ac8374f546edddca8
Nguyen Do <khoanguyen.do@outlook.com>
2024-07-28 04:23:44 +0700

    build: typecheck, font migrate and updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@vueuse/nuxt", "@nuxt/ui", "@pinia/nuxt"],
+  modules: ["@vueuse/nuxt", "@nuxt/ui", "@pinia/nuxt", "@nuxtjs/google-fonts"],
 
   runtimeConfig: {
-    tenorApiKey: process.env.TENOR_API_KEY,
     public: {
       pocketbaseUrl: process.env.POCKETBASE_URL,
     },
   },
 
   typescript: {
-    shim: false,
+    typeCheck: true,
   },
 
-  css: ["~/assets/fonts/inter.css"],
   devtools: { enabled: true },
 
-  ui: {
-    icons: ["fluent"],
-  },
-
-  experimental: {
-    appManifest: false,
+  googleFonts: {
+    families: {
+      Inter: "200..700",
+    },
   },
 
   compatibilityDate: "2024-07-09",
