e003a7d0ca80b219778059eabaa4722aa01300bb
Nguyen Do <khoanguyen.do@outlook.com>
2024-07-30 07:17:43 +0700

    fix(bulk): add more data to view (#76)

--- components/bulk/BulkRelease.vue
+++ components/bulk/BulkRelease.vue
@@ -4,6 +4,7 @@ import {
   type ReleasesResponse,
   type PublishersResponse,
   type TitlesResponse,
+  type FormatsResponse,
 } from "@/types/pb";
 import type { BulkData } from "@/stores/bulk";
 
@@ -16,12 +17,17 @@ async function releases(q: string) {
     ReleasesResponse<{
       publisher: PublishersResponse;
       partner: PublishersResponse;
-      title: TitlesResponse;
+      title: TitlesResponse<
+        unknown,
+        {
+          format: FormatsResponse;
+        }
+      >;
     }>
   >(1, 10, {
     filter: $pb.filter("title.name ~ {:q}", { q }),
-    sort: "-updated",
-    expand: "publisher, partner, title",
+    sort: "-created",
+    expand: "publisher, partner, title.format",
   });
 
   return releases.items;
@@ -47,15 +53,45 @@ function remove(i: number) {
         placeholder="Choose a release"
       >
         <template #option="{ option }">
+          <UAvatarGroup
+            size="2xs"
+            :max="2"
+            :ui="{ ring: 'ring-white dark:ring-gray-800' }"
+          >
+            <UAvatar
+              :src="
+                $pb.files.getUrl(
+                  option.expand?.publisher,
+                  option.expand?.publisher.logo,
+                )
+              "
+              :alt="option.expand?.publisher.name"
+            />
+            <UAvatar
+              v-if="option.expand?.partner"
+              :src="
+                $pb.files.getUrl(
+                  option.expand?.partner,
+                  option.expand?.partner.logo,
+                )
+              "
+              :alt="option.expand?.partner.name"
+            />
+          </UAvatarGroup>
           <span class="truncate">
             {{ option.name }}
-            <span
-              v-if="option.disambiguation"
-              class="text-gray-500 dark:text-gray-400"
-            >
-              {{ " " }}({{ option.disambiguation }})
-            </span>
           </span>
+          <span
+            v-if="option.disambiguation"
+            class="text-gray-500 dark:text-gray-400"
+          >
+            ({{ option.disambiguation }})
+          </span>
+          <span class="text-gray-500 dark:text-gray-400">
+            {{ option.expand.title.expand.format.name }}
+          </span>
+          <UBadge v-if="option.digital" color="red">Digital</UBadge>
+          <UBadge variant="soft">{{ option.status }}</UBadge>
         </template>
       </UInputMenu>
     </template>
