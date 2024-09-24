a46827aa4496710431022ec657b27028a9bf0146
Nguyen Do <khoanguyen.do@outlook.com>
2024-09-24 11:52:25 +0700

    feat: implement volume component

---
+++ components/AppVolume.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { volume } = defineProps<{ volume: number }>();
+</script>
+
+<template>
+  <UBadge color="gray">
+    <span v-if="volume == 0">Oneshot</span>
+    <span v-else-if="volume >= 90000000">Boxset</span>
+    <span v-else>
+      {{ Math.floor(volume / 10000) + (volume % 10) * 0.1 }}
+    </span>
+  </UBadge>
+</template>
