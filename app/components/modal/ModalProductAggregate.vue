eb63ddf44e0a1bcbee68fcd5f3b3f9cff4652029
Nguyen Do <khoanguyen.do@outlook.com>
2026-04-26 00:35:07 +0700

    feat: support fahasa search

--- app/components/modal/ModalProductAggregate.vue
+++ app/components/modal/ModalProductAggregate.vue
@@ -7,6 +7,9 @@ const emit = defineEmits<{
 
 const page = ref(0);
 
+const query = ref("");
+const queryDebounced = refDebounced(query, 200);
+
 const { data } = await useAsyncData(
   () =>
     $urano.listFahasaProduct({
@@ -16,21 +19,44 @@ const { data } = await useAsyncData(
     }),
   { server: false, lazy: true, watch: [page] },
 );
+
+const { data: searchData } = await useAsyncData(
+  () =>
+    $urano.searchFahasaProduct({
+      query: queryDebounced.value,
+    }),
+  { server: false, lazy: true, immediate: false, watch: [queryDebounced] },
+);
 </script>
 
 <template>
   <UModal title="Assets">
     <template #body>
-      <div v-if="data?.products" class="grid grid-cols-6 gap-3">
-        <div
-          v-for="product in data.products"
-          :key="product.id"
-          class="rounded w-full object-contain aspect-square bg-gray-200 dark:bg-gray-800 hover:brightness-75 transition-all hover:cursor-pointer"
-          @click="emit('close', product.id)"
-        >
-          <img :src="product.images[0]" />
-          {{ product.name }}
-        </div>
+      <UInput v-model="query" class="w-full" />
+      <div class="grid grid-cols-4 gap-3 mt-3">
+        <template v-if="searchData">
+          <div
+            v-for="product in searchData.products"
+            :key="product.id"
+            class="rounded w-full object-contain aspect-square bg-gray-200 dark:bg-gray-800 hover:brightness-75 transition-all hover:cursor-pointer"
+            @click="emit('close', product.id)"
+          >
+            <img :src="product.images[0]" />
+            {{ product.name }}
+          </div>
+        </template>
+
+        <template v-else-if="data?.products">
+          <div
+            v-for="product in data.products"
+            :key="product.id"
+            class="rounded w-full object-contain aspect-square bg-gray-200 dark:bg-gray-800 hover:brightness-75 transition-all hover:cursor-pointer"
+            @click="emit('close', product.id)"
+          >
+            <img :src="product.images[0]" />
+            {{ product.name }}
+          </div>
+        </template>
       </div>
 
       <UButton
